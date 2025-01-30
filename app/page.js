67d2275e4dc5b022755b33ba87aa7ab6b9eed092
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-01-30 23:07:04 +0800

    fix: change title & and link to article

--- app/page.js
+++ app/page.js
@@ -40,7 +40,7 @@ function Cover() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center px-4 md:px-8 lg:px-16 mt-10">
       <div className="text-center md:text-left">
-        <h1 className="text-[24px] sm:text-[48px] md:text-5xl font-extrabold">Transformasi Digital Dengan UMKM di Desa Teros</h1>
+        <h1 className="text-[20px] sm:text-[48px] md:text-[40px] font-extrabold">Pengembangan Kewirausahaan Melalui Pelatihan dan Pendampingan Digital Marketing di Desa Teros, Kecamatan Labuhan Haji, Lombok Timur</h1>
         <p className="text-[10px] sm:text-base text-gray-500 mt-2">
           Mahasiswa KKN PMD Universitas Mataram bekerja sama dengan UMKM di Desa Teros dalam melakukan transformasi digital pada setiap usaha UMKM dengan melakukan pengenalan terkait digital marketing dan re-branding.
         </p>
@@ -72,7 +72,20 @@ function Sosialisasi() {
             className="rounded-2xl object-cover object-bottom"
           />
         </div>
+
+        <div className="mt-6">
+          <p>Melalui inisiatif para mahasiswa KKN Pemberdayaan Masyarakat Desa (PMD) dari Universitas Mataram (UNRAM), digelar sosialisasi dan pelatihan pemasaran berbasis media sosial untuk mendukung UMKM setempat naik kelas.</p>
+          <a href="https://mataramradio.com/2025/01/17/dukung-umkm-desa-teros-mahasiswa-kkn-pmd-unram-gelar-pelatihan-pemasaran-digital-berbasis-media-sosial/#google_vignette" className=" underline">Baca Selengkapnya</a>
+        </div>
       </div>
     </div>
   );
+}
+
+function Branding() {
+  return (
+    <div>
+
+    </div>
+  )
 }
