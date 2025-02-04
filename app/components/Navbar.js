cb69e3aac806527acfe0a1abd454b3e58300334f
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-02-04 15:04:25 +0800

    fix: delete about page

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,11 +1,12 @@
+import Link from "next/link";
 
 export default function Navbar() {
     return (
         <div className="flex items-center gap-x-6 sm:gap-x-16 p-4 justify-center">
             <h1 className="text-[24px] sm:text-3xl font-bold">UMKM Desa Teros</h1>
-            <div className="flex gap-x-2 sm:gap-x-6 text-gray-500 text-[10px] sm:text-base">
-                <p>About</p>
-            </div>
+            {/* <div className="flex gap-x-2 sm:gap-x-6 text-gray-500 text-[10px] sm:text-base">
+                <Link href="/about">Tentang Kami</Link>
+            </div> */}
         </div>
     )
 }
